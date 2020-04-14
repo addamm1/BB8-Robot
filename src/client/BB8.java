1b024239265ddf276a5ddab0b69ffd8a8c1476db
addamm1 <arneulight@gmail.com>
2020-04-14 19:28:23 +0100

    Works after first hardware test

---
+++ src/client/BB8.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+public class BB8 {
+    private static Socket socket;
+    private static PrintWriter networkOut;
+    private static BufferedReader networkIn;
+    private static String host = "192.168.1.25";
+    private static int port = 50000;
+    private static long timeout = 5;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // setting up networking
+        socket = new Socket(host, port);
+	System.out.println("Connected");
+        networkOut = new PrintWriter(socket.getOutputStream(), true);
+        networkIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        runRobotClient();
+    }
+    private static void runRobotClient() throws InterruptedException, IOException {
+        while(!networkIn.ready()) {
+            networkOut.println("RIGHT FWD 128 LEFT FWD 128");
+            TimeUnit.SECONDS.sleep(timeout);
+            networkOut.println("RIGHT FWD 128 LEFT BWD 128");
+            TimeUnit.SECONDS.sleep(timeout);
+            networkOut.println("RIGHT BWD 128 LEFT FWD 128");
+            TimeUnit.SECONDS.sleep(timeout);
+            networkOut.println("RIGHT BWD 128 LEFT BWD 128");
+            TimeUnit.SECONDS.sleep(timeout);
+            networkOut.println("RIGHT FWD 128 LEFT FWD 128");
+            TimeUnit.SECONDS.sleep(timeout);
+            networkOut.println("RIGHT FWD 0 LEFT FWD 0");
+        }
+        networkOut.println("SHUTDOWN");
+    }
+}
---
+++ src/server/Motor.java
@@ -0,0 +1,53 @@
+import com.pi4j.io.gpio.*;
+
+public class Motor {
+    private boolean dir_fwd = true;
+    private int magnitude = 0;
+    private final GpioController gpio;
+    private GpioPinPwmOutput dir1;
+    private GpioPinPwmOutput dir2;
+    private int dir1PinNum;
+    private int dir2PinNum;
+
+    public Motor(GpioController gpio, int dir1PinNum, int dir2PinNum){
+        this.dir1PinNum = dir1PinNum;
+        this.dir2PinNum = dir2PinNum;
+        this.gpio = gpio;
+        dir1 = this.gpio.provisionPwmOutputPin(RaspiPin.getPinByAddress(this.dir1PinNum));
+        dir2 = this.gpio.provisionPwmOutputPin(RaspiPin.getPinByAddress(this.dir2PinNum));
+        dir1.setShutdownOptions(true, PinState.LOW);
+        dir2.setShutdownOptions(true, PinState.LOW);
+        dir1.setPwm(0);
+        dir2.setPwm(0);
+    }
+
+    public void changeDir(String dir){
+        dir_fwd = dir.equals("FWD");
+    }
+
+    public void changeMagnitude(int magnitude){
+        this.magnitude = magnitude;
+    }
+    public void updatePins(){
+        if(magnitude == 0){
+            dir1.setPwm(0);
+            dir2.setPwm(0);
+        }
+        else if (magnitude >= 0 && magnitude <= 255){
+            if(dir_fwd){
+                dir1.setPwm(magnitude);
+                dir2.setPwm(0);
+            }
+            else{
+                dir1.setPwm(0);
+                dir2.setPwm(magnitude);
+            }
+        }
+        else{
+            exit();
+        }
+    }
+    public void exit(){
+        gpio.shutdown();
+    }
+}
